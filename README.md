f6381c39ea34a5e522e07ec7a65ab2a9e85fecb9
Tom Ritchford <tom@swirly.com>
2018-06-07 17:19:47 +0200

    Add unit tests for spam checking

--- README.md
+++ README.md
@@ -12,13 +12,15 @@ Dragging documents and directories onto the artHash application generates a uniq
 
 USAGE:
 
-1.) Download the project. 
+1.) Download the project.
 
 Direct link: https://github.com/rec/arthash/archive/master.zip
 
 2.) The archive contains a directory that has an Automator application in it called "arthash".   It looks like a robot. Drag it to wherever you wish.
 
-3.) Drag and drop the artwork you wish to hash on the robot icon. The hash is saved in the same folder, and with the same name as the source file with ".sha256.txt" appended. 
-NOTES: 
+3.) Drag and drop the artwork you wish to hash on the robot icon. The hash is saved in the same folder, and with the same name as the source file with ".sha256.txt" appended.
+
+NOTES:
 • If Mac says '"arthash" can't be opened because it is from an unidentified developer', open System Preferences / Security & Privacy / General and click on "Open Anyway" or change your security preferences to allow apps dowloaded from "Anywhere"
+
 • Small files hash quickly, large files take some time.
---
+++ python/arthash/check_spam.py
@@ -0,0 +1,70 @@
+"""
+
+Detect malformed hashes.
+Attempt to detect spam hashes.
+
+"""
+
+import collections, math, hashlib, gzip
+
+HASH_LENGTH = 64
+HASH_CHARS = frozenset('0123456789abcdef')
+MINIMUM_ENTROPY = 3
+ENTROPY_ERROR = 'Entropy was less than %s' % MINIMUM_ENTROPY
+
+
+def possible_hash(s):
+    return len(s) == HASH_LENGTH and all(c in HASH_CHARS for c in s)
+
+
+def entropy(string):
+    "Calculates the Shannon entropy of a string"
+    # get probability of chars in string
+    key_dict = dict.fromkeys(list(string))
+    prob = [string.count(c) / len(string) for c in key_dict]
+
+    # calculate the entropy
+    return -sum(p * math.log(p) / math.log(2) for p in prob)
+
+
+def entropy_ideal(length):
+    "Calculates the ideal Shannon entropy of a string with given length"
+
+    prob = 1.0 / length
+    return -1.0 * length * prob * math.log(prob) / math.log(2.0)
+
+
+def entropy_hashes(count, value=b'x'):
+    # 100000: (3.411598784883635, 3.977217001462483)
+    # 1000000: (3.326479333192638, 3.9829127510968623)
+    h = hashlib.sha256()
+    min_entropy = 10000000000
+    max_entropy = -1
+
+    for i in range(count):
+        e = entropy(h.hexdigest())
+        min_entropy = min(e, min_entropy)
+        max_entropy = max(e, max_entropy)
+        h.update(value)
+    return min_entropy, max_entropy
+
+
+def generate_hashes(count, value=b'x'):
+    h = hashlib.sha256()
+
+    min_count = 65
+    max_count = -1
+
+    for i in range(count):
+        for k, v in collections.Counter(h.hexdigest()).items():
+            min_count = min(min_count, v)
+            max_count = max(max_count, v)
+        h.update(value)
+
+    return min_count, max_count
+
+
+def check(s):
+    e = entropy(s)
+    if e < MINIMUM_ENTROPY:
+        raise ValueError(ENTROPY_ERROR, e, s)
