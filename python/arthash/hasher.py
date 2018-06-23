e138f5f4d7003b8308ca04bf7e215443e9b1760f
Tom Ritchford <tom@swirly.com>
2018-06-23 15:57:56 +0200

    Make two functions in hasher.py protected

--- python/arthash/hasher.py
+++ python/arthash/hasher.py
@@ -14,7 +14,7 @@ EXCLUDED_PREFIXES = '.'
 def hasher(root, chunksize):
     h = HASH_CLASS()
 
-    for filename in sorted(walk(root)):
+    for filename in sorted(_walk(root)):
         h.update(filename.encode())
 
         with open(os.path.join(root, filename), 'rb') as f:
@@ -26,15 +26,15 @@ def hasher(root, chunksize):
     return h.hexdigest()
 
 
-def exclude(files):
+def _walk(root):
+    for dirpath, dirnames, filenames in os.walk(root):
+        dirnames[:] = _exclude(dirnames)
+        for f in _exclude(filenames):
+            path = os.path.join(dirpath, f)
+            yield os.path.relpath(path, root)
+
+
+def _exclude(files):
     for f in files:
         if not any(f.startswith(p) for p in EXCLUDED_PREFIXES):
             yield f
-
-
-def walk(root):
-    for dirpath, dirnames, filenames in os.walk(root):
-        dirnames[:] = exclude(dirnames)
-        for f in exclude(filenames):
-            path = os.path.join(dirpath, f)
-            yield os.path.relpath(path, root)
