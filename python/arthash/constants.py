8584d99913dca1f54b69b0e2c20809a304bdbc60
Tom Ritchford <tom@swirly.com>
2018-06-23 15:45:41 +0200

    Rename file_hasher and add features to it

--- python/arthash/constants.py
+++ python/arthash/constants.py
@@ -7,3 +7,9 @@ IP_ADDRESS = DEBUG_IP_ADDRESS if DEBUG else FINAL_IP_ADDRESS
 PORT = 7887
 
 PATH = ''
+
+"""
+The size of file chunks we read and hash.  The arthash computation is
+independent of the CHUNKSIZE setting.
+"""
+CHUNKSIZE = 100000000
