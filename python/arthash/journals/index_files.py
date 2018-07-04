a2193cfc6120c0be3b0a06262d53b1f6d835de4d
Tom Ritchford <tom@swirly.com>
2018-07-04 12:52:45 +0200

    Move code from journal_files.py to index_file.py

--- python/arthash/journals/index_files.py
+++ python/arthash/journals/index_files.py
@@ -20,3 +20,31 @@ def link_lines(directory):
 
     yield '    </tr>'
     yield '</table>'
+
+
+def write_indexes(journal_file):
+    print('Created', journal_file)
+    directory = journal_file
+    for title in TITLES:
+        directory = os.path.dirname(directory)
+        body = '\n'.join(link_lines(directory))
+        index_filename = os.path.join(directory, 'index.html')
+        with open(index_filename, 'w') as fp:
+            fp.write(DOC_TEMPLATE.format(title=title, body=body))
+
+
+TITLES = (
+    'artHash index page level 0',
+    'artHash index page level 1',
+    'artHash index page level 2',
+    'artHash index page level 3',
+)
+
+DOC_TEMPLATE = """<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html><head>
+<title>{title}</title>
+</head>
+<body>
+{body}
+</body> </html>
+"""
--- python/arthash/journals/journal_files.py
+++ python/arthash/journals/journal_files.py
@@ -11,36 +11,8 @@ def write(journal_file, page):
         json.dump(page, fp, indent=2)
 
     if not exists:
-        _write_indexes(journal_file)
+        index_files.write_indexes(journal_file)
 
 
 def read(journal_file):
     return json.load(open(journal_file))
-
-
-def _write_indexes(journal_file):
-    print('Created', journal_file)
-    directory = journal_file
-    for title in TITLES:
-        directory = os.path.dirname(directory)
-        body = '\n'.join(index_files.link_lines(directory))
-        index_filename = os.path.join(directory, 'index.html')
-        with open(index_filename, 'w') as fp:
-            fp.write(DOC_TEMPLATE.format(title=title, body=body))
-
-
-TITLES = (
-    'artHash index page level 0',
-    'artHash index page level 1',
-    'artHash index page level 2',
-    'artHash index page level 3',
-)
-
-DOC_TEMPLATE = """<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
-<html><head>
-<title>{title}</title>
-</head>
-<body>
-{body}
-</body> </html>
-"""
