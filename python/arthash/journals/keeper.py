45a7ad50066c6e062d3e267e9d4e2fd14610446a
Tom Ritchford <tom@swirly.com>
2018-07-04 13:00:03 +0200

    Inline journal_files.py into keeper.py

--- python/arthash/journals/keeper.py
+++ python/arthash/journals/keeper.py
@@ -1,5 +1,5 @@
-import datetime, os
-from . import journal_files
+import datetime, json, os
+from . import index_files
 from . import organization as org
 from .. util import files
 
@@ -10,15 +10,18 @@ class Keeper:
     """
     def __init__(self, root, organization=None):
         self.org = organization or org.Organization()
-        os.makedirs(root, exist_ok=True)
         self.root = root
+
+        os.makedirs(root, exist_ok=True)
         self.last = files.last_file(root)
+
         if self.last:
-            self.page = journal_files.read(self.last)
+            self.page = json.load(open(self.last))
         else:
             segment = self.org.digit_format % 0
             parts = '/'.join([segment] * self.org.levels)
-            self._set_last(os.path.join(self.root, parts + '.json'))
+            first = os.path.join(self.root, parts + '.json')
+            self._set_last(first)
 
     def add_hash(self, arthash):
         if len(self.page) >= self.org.page_size:
@@ -27,7 +30,16 @@ class Keeper:
             self._set_last(os.path.join(self.root, next_parts))
 
         self.page.append([arthash, timestamp()])
-        journal_files.write(self.last, self.page)
+
+        exists = os.path.exists(self.last)
+        if not exists:
+            os.makedirs(os.path.dirname(self.last), exist_ok=True)
+
+        with open(self.last, 'w') as fp:
+            json.dump(self.page, fp, indent=2)
+
+        if not exists:
+            index_files.write_indexes(self.last)
 
     def _set_last(self, last):
         self.last = last
